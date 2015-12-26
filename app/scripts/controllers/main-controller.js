067474a822ce780706078bd94335d4ac09aa19be
Peter Velkov <kidroca@gmail.com>
2015-12-26 14:30:05 +0200

    Cleared the example controller code from the main controller

--- app/scripts/controllers/main-controller.js
+++ app/scripts/controllers/main-controller.js
@@ -1,37 +1,13 @@
-(function() {
+(function(){
 	'use strict';
 
-	function MainController($scope, ls) {
-		var self = this,
-			todosInStorage = ls.get('todos');
+	function MainController() {
+		var self = this;
 
-		self.todos = todosInStorage || [];
 
-		$scope.$watch('ctrl.todos', function syncStorageTodos() {
-			ls.set('todos', self.todos);
-		}, true);
-
-		self.addTodo = function addTodo() {
-			self.todos.push($scope.todo);
-			$scope.todo = '';
-		};
-
-		self.removeTodo = function(index) {
-			self.todos.splice(index, 1);
-		};
-
-		self.log = function log() {
-			console.log($scope);
-		};
 	}
-	
-	/**
-	 * @ngdoc function
-	 * @name quizProjectApp.controller:MainCtrl
-	 * @description
-	 * # MainCtrl
-	 * Controller of the quizProjectApp
-	 */
-	angular.module('quizProjectApp')
-		.controller('MainController', ['$scope', 'localStorageService', MainController]);
+
+	angular.module('quizProjectApp.controllers')
+		.controller('MainController', [MainController]);
+		
 }());
