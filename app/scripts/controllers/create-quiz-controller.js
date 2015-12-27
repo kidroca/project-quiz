bb96f2e818619a28eebbefd9c89a0252ef2f6d9d
Peter Velkov <kidroca@gmail.com>
2015-12-27 10:51:49 +0200

    Updated the Create quiz controller to call the popup modal

--- app/scripts/controllers/create-quiz-controller.js
+++ app/scripts/controllers/create-quiz-controller.js
@@ -1,21 +1,51 @@
 (function() {
 	'use strict';
 
-	function CreateQuizController($scope, quizStorage) {
+	// Todo: display a list of the created questions with ng-repeat
+	// Todo: selecting a question from the list opens edit menu -> modal
+
+	function CreateQuizController($scope, $uibModal, quizStorage) {
 		var self = this;
-		
+
+		self.bonus = {
+			title: 'spinderman',
+			answers: [{text: 'as', letter: 'a'}, {text: 'bas', letter: 'b'}]
+		};
+
+		self.quiz = {
+			title: '',
+			description: '',
+			questions: []
+		};
+
 		self.addQuiz = function addQuiz(quiz) {
 			quizStorage.addQuiz(quiz);
 		};
+
+		self.addQuestion = function addQuestion(question) {
+			var modalInstance = $uibModal.open({
+				animation: true,
+				templateUrl: 'views/add-question.html',
+				controller: 'AddQuestionController',
+				controllerAs: 'ctrl',
+				resolve: {
+					items: question || self.bonus
+				}
+			});
+
+			modalInstance.result.then(function(question) {
+				if (question !== null) {
+					self.quiz.questions.push(question);
+				}
+
+				console.log(self.quiz.questions);
+			}, function() {
+				console.log('Modal dismissed at: ' + new Date());
+				console.log(self.bonus);
+			});
+		};
 	}
-	
-	/**
-	 * @ngdoc function
-	 * @name quizProjectApp.controller:MainCtrl
-	 * @description
-	 * # MainCtrl
-	 * Controller of the quizProjectApp
-	 */
+
 	angular.module('quizProjectApp.controllers')
-		.controller('CreateQuizController', ['$scope', 'quizStorageService', CreateQuizController]);
+		.controller('CreateQuizController', ['$scope', '$uibModal', 'quizStorageService', CreateQuizController]);
 }());
