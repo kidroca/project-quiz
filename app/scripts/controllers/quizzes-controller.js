35a75ed689e196ca3e0d26a6fa51dbffcda65ac7
Peter Velkov <kidroca@gmail.com>
2015-12-29 11:53:00 +0200

    Updated quizzes view and controller

--- app/scripts/controllers/quizzes-controller.js
+++ app/scripts/controllers/quizzes-controller.js
@@ -5,24 +5,13 @@
 		var self = this;
 
 		self.init = function init() {
-			self.quizzes = quizStorageService.getQuizzes();
+			$scope.$storage = quizStorageService.getQuizzes();
+			console.log($scope.$storage);
 		};
 
 		$scope.myInterval = 5000;
 		$scope.noWrapSlides = false;
-		var slides = $scope.slides = [];
-		$scope.addSlide = function() {
-			var newWidth = 600 + slides.length + 1;
-			slides.push({
-				image: '//placekitten.com/' + newWidth + '/300',
-				text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' +
-					['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-			});
-		};
-		for (var i = 0; i < 4; i++) {
-			$scope.addSlide();
-		}
-
+		
 		self.init();
 	}
 
