db5e2f9d728802f7f261aa0e62c6025b85ddad0f
Peter Velkov <kidroca@gmail.com>
2015-12-31 01:18:55 +0200

    edited the main controller to handle the start page correctly

--- app/scripts/controllers/main-controller.js
+++ app/scripts/controllers/main-controller.js
@@ -1,13 +1,32 @@
 (function(){
 	'use strict';
 
-	function MainController() {
+	function MainController($location, identity, auth) {
 		var self = this;
 
+        function waitForLogin() {
+            console.log('waiting for login...');
 
+            identity.getUser()
+            .then(function (user) {
+                self.currentUserGlobal = user;
+                console.log('ready');
+            });
+        }
+
+        self.logout = function () {
+            console.log('Logging out...');
+            auth.logout();
+            self.currentUserGlobal = null;
+            waitForLogin();
+
+            $location.path('/');
+        };
+
+        waitForLogin();
 	}
 
 	angular.module('quizProjectApp.controllers')
-		.controller('MainController', [MainController]);
+		.controller('MainController', ['$location', 'identity', 'auth', MainController]);
 		
 }());
