16011ed71352209d0409e77b0fac0b489858847d
Peter Velkov <kidroca@gmail.com>
2016-01-03 01:59:12 +0200

    added controller methods for deleting and solving quizzes, binded methods to buttons, updated styles

--- app/scripts/quizzes/quizzes-controller.js
+++ app/scripts/quizzes/quizzes-controller.js
@@ -4,7 +4,7 @@
 	// Todo: Delete quiz button
 	// Todo: Take Quiz button and page
 
-	function QuizzesController($scope, $sessionStorage, $location, quizData) {
+	function QuizzesController($scope, $sessionStorage, $location, quizData, identity) {
 		var self = this;
 
 		self.init = function init() {
@@ -12,16 +12,48 @@
 				.then(function(result) {
 					$scope.$storage = result;
 				});
+
+			identity.getUser()
+				.then(function(result) {
+					$scope.userDetails = result;
+				});
 		};
 
 		self.editActive = function editActive () {
-			var quiz = $scope.$storage.filter(function(item) {
-				return item.active;
-			})[0];
+			var quiz = self.getActiveQuiz();
 
 			console.log(quiz);
 			$sessionStorage.editQuiz = quiz;
 			$location.path('/quizzes/edit');
+		};
+
+		self.deleteActive = function deleteActive () {
+			var i = 0;
+
+			$scope.$storage.find(function(element, index) {
+				i = index;
+				return element.active;
+			});
+
+			quizData.removeQuiz($scope.$storage[i].id)
+				.then(function() {
+					$scope.$storage.splice(i, 1);
+				});
+		};
+
+		self.solveAlctive = function solveAlctive () {
+			var quiz = self.getActiveQuiz();
+			
+			$sessionStorage.solveQuiz = quiz;
+			$location.path('/quizzes/solve/' + quiz.id);
+		};
+
+		self.getActiveQuiz = function getActiveQuiz () {
+			var quiz = $scope.$storage.filter(function(item) {
+				return item.active;
+			})[0];
+
+			return quiz;
 		};
 
 		$scope.noWrapSlides = false;
@@ -31,5 +63,11 @@
 
 	angular.module('quizProjectApp.controllers')
 		.controller('QuizzesController', 
-			['$scope', '$sessionStorage', '$location', 'QuizDataService', QuizzesController]);
+			[
+			'$scope', 
+			'$sessionStorage', 
+			'$location', 
+			'QuizDataService',
+			'identity',
+			 QuizzesController]);
 }());
